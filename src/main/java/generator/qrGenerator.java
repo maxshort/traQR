c6bc42fe65e035d1a330fbd1e53104bda08c8294
docspock <christopher.p.bartz@gmail.com>
2015-07-14 14:40:23 -0500

    Fixed formatting

--- src/main/java/generator/qrGenerator.java
+++ src/main/java/generator/qrGenerator.java
@@ -16,34 +16,35 @@ import com.google.zxing.common.BitMatrix;
 public class qrGenerator {
 	private static final BarcodeFormat DEFAULT_BARCODE = BarcodeFormat.QR_CODE;
 	private final static String BASE_URL = "//servername/?=";
-	private final static String OUTPUT_FOLDER = System.getProperty("user.home"); //Some directory
+	private final static String OUTPUT_FOLDER = System.getProperty("user.home"); // Some
+																					// directory
 	private final static String IMAGE_FORMAT = "PNG";
 	private final static int width = 300;
 	private final static int height = 300;
-	
+
 	private qrGenerator() {
-		
+
 	}
-	
+
 	/**
-	 * Static method to create a QR code.
-	 * The default URL is {@code //servername/?="[imageId]"}.
-	 * The file is saved to the user's home directory as "[imageId].png".
-	 * @param imageId The ID number of the QR code to generate.
-	 * @throws Exception On any error.
+	 * Static method to create a QR code. The default URL is
+	 * {@code //servername/?="[imageId]"}. The file is saved to the user's home
+	 * directory as "[imageId].png".
+	 * 
+	 * @param imageId
+	 *            The ID number of the QR code to generate.
+	 * @throws Exception
+	 *             On any error.
 	 */
 	public static void createQR(int imageId) throws Exception {
-		
-		
-		String idString = Integer.toString(imageId); 
-		
-		
+
+		String idString = Integer.toString(imageId);
+
 		String contents = BASE_URL + "\"" + idString + "\"";
 		String outFileString = OUTPUT_FOLDER + File.separator + idString + ".png";
-		
-				
+
 		BitMatrix matrix = new MultiFormatWriter().encode(contents, DEFAULT_BARCODE, width, height);
-	    MatrixToImageWriter.writeToPath(matrix, IMAGE_FORMAT, Paths.get(outFileString));
-	    System.out.println("Image saved to " + outFileString + ".");
-	}		
+		MatrixToImageWriter.writeToPath(matrix, IMAGE_FORMAT, Paths.get(outFileString));
+		System.out.println("Image saved to " + outFileString + ".");
+	}
 }
