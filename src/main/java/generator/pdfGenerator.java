53573b527b8f9e9217b8c84726829f105d4ec0b6
docspock <christopher.p.bartz@gmail.com>
2015-07-16 11:03:44 -0500

    Finalized PDF rendering.

--- src/main/java/generator/pdfGenerator.java
+++ src/main/java/generator/pdfGenerator.java
@@ -20,76 +20,59 @@ public class pdfGenerator {
 	}
 
 	public static void createPDF(String locationName, int locationID) throws IOException, COSVisitorException {
+		pdfGenerator generator = new pdfGenerator();
+		generator.PDFgenerator(locationName, locationID);
+	}
+
+	private void PDFgenerator(String locationName, int locationID) throws IOException, COSVisitorException {
 		String fileLocationString = OUTPUT_FOLDER + File.separator + Integer.toString(locationID);
 
 		PDDocument document = new PDDocument();
 		PDPage page = new PDPage(PDPage.PAGE_SIZE_LETTER);
 
-		PDFont font1 = PDType1Font.HELVETICA_BOLD_OBLIQUE;
-		PDFont font2 = PDType1Font.HELVETICA_BOLD;
-		PDFont font3 = PDType1Font.HELVETICA_OBLIQUE;
-
-		int font1Size = 24;
-		int font2Size = 20;
-		int font3Size = 22;
-
 		document.addPage(page);
 
 		// Text:
-		String line1 = "Need some directions?";
-		float line1X = centerTextOnPageX(page, font1, font1Size, line1);
-		float line1Y = centerTextOnPageYFractional(page, font1, font1Size, line1, 1, 6);
-
-		String line2 = "Just ask the new interns!";
-		float line2X = centerTextOnPageX(page, font2, font2Size, line2);
-		float line2Y = centerTextOnPageYFractional(page, font2, font2Size, line2, 2, 6);
-
-		String line3 = "TraQR (A project of #CernerHackfest2015)";
-		float line3X = centerTextOnPageX(page, font3, font3Size, line3);
-		float line3Y = centerTextOnPageYFractional(page, font3, font3Size, line3, 5, 6);
+		TextObject line1 = new TextObject("Need some directions?", page, PDType1Font.HELVETICA_BOLD_OBLIQUE, 42);
+		TextObject line2 = new TextObject("Just ask the new interns!", page, PDType1Font.HELVETICA_BOLD, 50);
+		TextObject line3 = new TextObject("TraQR (A project of #CernerHackfest2015)", page,
+				PDType1Font.HELVETICA_BOLD_OBLIQUE, 22);
 
 		// QR code:
 		InputStream in = new FileInputStream(new File(fileLocationString + ".jpg"));
 		PDJpeg img = new PDJpeg(document, in);
 		float imgX = centerOnPageX(page, img.getWidth());
-		float imgY = centerOnPageYFractional(page, img.getHeight(), 1, 2);
+		float imgY = centerOnPageY(page, img.getHeight());
 
 		// Render the page:
 		PDPageContentStream contentStream = new PDPageContentStream(document, page);
 
-		contentStream.beginText();
+		// Write lines.
+		writeLine(contentStream, line1, 1, 8);
+		writeLine(contentStream, line2, 1, 4);
+		writeLine(contentStream, line3, 7, 8);
 
-		// Line1
-		contentStream.setFont(font1, font1Size);
-		contentStream.moveTextPositionByAmount(line1X, line1Y);
-		contentStream.drawString(line1);
+		// Close text stream
 
-		// Line2
-		contentStream.setFont(font2, font2Size);
-		contentStream.moveTextPositionByAmount(line2X, line2Y);
-		contentStream.drawString(line2);
-
-		// Line3
-		contentStream.setFont(font3, font3Size);
-		contentStream.moveTextPositionByAmount(line3X, line3Y);
-		contentStream.drawString(line3);
-		contentStream.endText();
-
-		// contentStream.drawImage(img, imgX, imgY);
+		contentStream.drawImage(img, imgX, imgY);
 		contentStream.close();
 
 		document.save(fileLocationString + ".pdf");
 	}
 
-	static float centerTextOnPageX(PDPage page, PDFont font, int fontSize, String line) throws IOException {
-		float lineWidth = font.getStringWidth(line) / 1000 * fontSize;
-		return centerOnPageX(page, lineWidth) - (lineWidth / 2);
+	static void writeLine(PDPageContentStream contentStream, TextObject t) throws IOException {
+		writeLine(contentStream, t, 1, 2);
 	}
 
-	static float centerTextOnPageYFractional(PDPage page, PDFont font, int fontSize, String line, int numerator,
-			int denominator) {
-		float lineHeight = font.getFontDescriptor().getFontBoundingBox().getHeight() / 1000 * fontSize;
-		return centerOnPageYFractional(page, lineHeight, numerator, denominator);
+	static void writeLine(PDPageContentStream contentStream, TextObject t, int numerator, int denominator)
+			throws IOException {
+		contentStream.beginText();
+		contentStream.setFont(t.getFont(), t.getFontSize());
+		contentStream.moveTextPositionByAmount(t.getCenteredXCoordinate(),
+				t.getFractionalYCoordinate(numerator, denominator));
+		contentStream.drawString(t.getText());
+		contentStream.endText();
+
 	}
 
 	static float centerOnPageX(PDPage page, float objectWidth) {
@@ -97,7 +80,48 @@ public class pdfGenerator {
 		return (page.getMediaBox().getWidth() - objectWidth) / 2;
 	}
 
-	static float centerOnPageYFractional(PDPage page, float objectHeight, int numerator, int denominator) {
-		return ((page.getMediaBox().getHeight() - objectHeight) * (numerator / (float) denominator));
+	static float centerOnPageY(PDPage page, float objectHeight) {
+		return (page.getMediaBox().getHeight() - objectHeight) / 2;
+	}
+
+	class TextObject {
+		String line;
+		PDPage page;
+		PDFont font;
+		float width;
+		float height;
+		int fontSize;
+
+		// An immutable class
+		TextObject(String line, PDPage page, PDFont font, int fontSize) throws IOException {
+			this.line = line;
+			this.font = font;
+			this.page = page;
+			this.width = font.getStringWidth(line) / 1000 * fontSize;
+			this.height = font.getFontDescriptor().getFontBoundingBox().getHeight() / 1000 * fontSize;
+			this.fontSize = fontSize;
+		}
+
+		PDFont getFont() {
+			return font;
+		}
+
+		int getFontSize() {
+			return fontSize;
+		}
+
+		String getText() {
+			return line;
+		}
+
+		float getCenteredXCoordinate() {
+			float pageWidth = page.getMediaBox().getWidth();
+			return (pageWidth - width) / 2;
+		}
+
+		float getFractionalYCoordinate(int numerator, int denominator) {
+			float pageHeight = page.getMediaBox().getHeight();
+			return pageHeight - (((pageHeight - height) * numerator) / denominator);
+		}
 	}
 }
