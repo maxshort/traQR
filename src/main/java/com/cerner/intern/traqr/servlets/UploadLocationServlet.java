f01a4a0ac9178dd38cfd78ead686f42831bbb474
docspock <christopher.p.bartz@gmail.com>
2015-07-17 15:46:05 -0500

    Added a way to add locations.

--- src/main/java/com/cerner/intern/traqr/servlets/UploadLocationServlet.java
+++ src/main/java/com/cerner/intern/traqr/servlets/UploadLocationServlet.java
@@ -1,17 +1,23 @@
 package com.cerner.intern.traqr.servlets;
 
-import com.cerner.intern.traqr.core.Location;
-import com.cerner.intern.traqr.db.Database;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.sql.SQLException;
-import java.util.Map;
+
+import com.cerner.intern.traqr.core.Location;
+import com.cerner.intern.traqr.db.Database;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateExceptionHandler;
 
 /**
  * Created on 7/15/15.
@@ -19,28 +25,52 @@ import java.util.Map;
 @MultipartConfig
 public class UploadLocationServlet extends HttpServlet {
 
-    private final Map<Integer, Location> locations;
+	private final Map<Integer, Location> locations;
 
-    public UploadLocationServlet(Map<Integer, Location> locations) {
-        this.locations = locations;
-    }
+	public UploadLocationServlet(Map<Integer, Location> locations) {
+		this.locations = locations;
+	}
 
-    @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		Configuration config = new Configuration(Configuration.VERSION_2_3_22);
 
-        System.out.println(request.getParameterMap());
-        String name = request.getParameter("name"); //todo: db uses prepared statements??
-        System.out.println("NAME HERE: "+name);
-        try {
-            Location l = Database.insertLocation(name);
-            locations.put(l.getId(), l);
-            System.out.println(l);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+		// Code for grabbing template from stream
+		config.setClassForTemplateLoading(this.getClass(), "/");
+		config.setDefaultEncoding("UTF-8");
+		config.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
 
-        //TODO: ADD TO LIST OF LOCATIONS AND DB -- NEED DB TO KNOW ID
-        //TODO: FORWARD ON TO QR PAGE???
-    }
+		Template template = config.getTemplate("addLocation.ftl");
+
+		Map<String, Object> root = new HashMap<>();
+		root.put("locations", locations.values());
+
+		try {
+			template.process(root, response.getWriter());
+		} catch (TemplateException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+
+	}
+
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+		System.out.println(request.getParameterMap());
+		String name = request.getParameter("name"); // todo: db uses prepared
+													// statements??
+		System.out.println("NAME HERE: " + name);
+		try {
+			Location l = Database.insertLocation(name);
+			locations.put(l.getId(), l);
+			System.out.println(l);
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+
+		// TODO: ADD TO LIST OF LOCATIONS AND DB -- NEED DB TO KNOW ID
+		// TODO: FORWARD ON TO QR PAGE???
+	}
 
 }
---
+++ src/main/resources/addLocation.ftl
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Connect two locations</title>
+	</head>
+	<style>
+		
+	</style>
+	
+	<body>
+		<form method="POST" action="/locations">
+			<label for="name">What is the name of your location?</label>	
+			<br>
+			<br>	
+			<input name="name"  type="text" id="name" size="40" />
+			<input  type="submit" value="Submit"/>
+		</form>
+	</body>
+
+</html
